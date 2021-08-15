53571404693bd67079df921d1df1da9dd81f28b3
Pankaj Singh <47411477+pankisingh@users.noreply.github.com>
2021-08-15 12:54:07 +0530

    removed aws access key

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA6H5SETQS63VBQLO2"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 
 resource "aws_instance" "web" {
