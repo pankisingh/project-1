3a5503f3eda8147ae8d3025e070f55058e47d306
Pankaj Singh <47411477+pankisingh@users.noreply.github.com>
2021-08-19 23:10:42 +0530

    terraform lock

--- main.tf
+++ main.tf
@@ -1,13 +1,21 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = ""
-  secret_key = ""
+    region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-backend-spovedd"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "spovedd-lock"
+  }
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-085925f297f89fce1"
+  ami           = "ami-0947d2ba12ee1ff75"
   instance_type = "t2.micro"
-   tags = {
-    Name = "ubuntu"
+
+  tags = {
+    Name = "HelloWorld"
   }
 }
